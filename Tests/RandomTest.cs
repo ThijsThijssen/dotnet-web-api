2c732831be229b7103903ed279be8b0c83fb6b4c
ThijsThijssen <thijsthijssen@outlook.com>
2020-09-17 08:18:06 +0200

    added tests for testing ci/cd pipeline

---
+++ Tests/RandomTest.cs
@@ -0,0 +1,16 @@
+using Xunit;
+
+namespace TodoApi.Tests
+{
+    public class RandomTest {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        int Add(int x, int y) {
+            return x + y;
+        }
+    }
+}

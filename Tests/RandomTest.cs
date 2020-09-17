3c52efdeaf46ed6605d3efeb6284b8b32738bb09
ThijsThijssen <thijsthijssen@outlook.com>
2020-09-17 11:47:02 +0200

    new changes

--- Tests/RandomTest.cs
+++ Tests/RandomTest.cs
@@ -6,7 +6,7 @@ namespace TodoApi.Tests
         [Fact]
         public void PassingTest()
         {
-            Assert.Equal(4, Add(2, 2));
+            Assert.Equal(6, Add(4, 2));
         }
 
         int Add(int x, int y) {
